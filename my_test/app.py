5502feb2fd6d17fca7293dfc682092ee383f68d9
Sergey <40183830+SergV77@users.noreply.github.com>
2021-04-14 14:41:45 +0300

    Update app.py

--- my_test/app.py
+++ my_test/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def test():
-    return 'Мое Flask приложение в докере заработало'
+    return 'Мое Flask приложение в докере зарабо     тало'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
