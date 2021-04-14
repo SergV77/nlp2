b42788a245bc7ff82a06984fcf274c01f5e7ea10
Sergey <40183830+SergV77@users.noreply.github.com>
2021-04-14 18:20:54 +0300

    Update locLib.py

--- my_test/locLib.py
+++ my_test/locLib.py
@@ -3,18 +3,8 @@
 import os # Модуль функций для работы с операционной системой, не зависящие от используемой операционной системы
 import collections #Модуль специализированных типов данных, на основе словарей , кортежей , множеств , списков
 import math # Библиотека математических функций
-import numpy as np # Библиотека работы с массивами
 import requests
 import json
-import pickle
-
-from tensorflow.keras import utils # Утилиты для to_categorical
-from tensorflow.keras.models import load_model # загрузка сохраненных моделей
-
-
-from sklearn.preprocessing import LabelEncoder, StandardScaler # Функции для нормализации данных
-from sklearn import preprocessing # Пакет предварительной обработки данных
-from sklearn.model_selection import train_test_split
 
 from flask import Flask
 from flask import request
