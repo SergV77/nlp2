a2a104f795b3b9b9d6b4999f2fece312d6f719d9
Sergey <40183830+SergV77@users.noreply.github.com>
2021-04-16 12:25:29 +0300

    Update locLib.py

--- my_test/locLib.py
+++ my_test/locLib.py
@@ -3,12 +3,9 @@
 import os # Модуль функций для работы с операционной системой, не зависящие от используемой операционной системы
 import collections #Модуль специализированных типов данных, на основе словарей , кортежей , множеств , списков
 import math # Библиотека математических функций
-import requests
 import json
-import pickle
 
-from flask import Flask
+from flask imporet Flask
 from flask import request
 
-import numpy as np
-from tensorflow.keras.models import load_model
+
